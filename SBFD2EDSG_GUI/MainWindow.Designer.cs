81b9bdaf6ebf7a23f37575e6673a1568e1c40c8c
Montablan64 <Montablan64@DESKTOP-IQTVTEF>
2025-11-30 01:53:17 +0800

    Add project files.

---
+++ SBFD2EDSG_GUI/MainWindow.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace SBFD2EDSG_GUI
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.program_title = new System.Windows.Forms.Label();
+            this.open_file_button = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.output_textbox = new System.Windows.Forms.TextBox();
+            this.description_label = new System.Windows.Forms.Label();
+            this.copy_output_button = new System.Windows.Forms.Button();
+            this.log_geneated_text_button = new System.Windows.Forms.CheckBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.code_tab_page = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.output_code_textbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.code_tab_page.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // program_title
+            // 
+            this.program_title.AutoSize = true;
+            this.program_title.Location = new System.Drawing.Point(13, 13);
+            this.program_title.Name = "program_title";
+            this.program_title.Size = new System.Drawing.Size(39, 13);
+            this.program_title.TabIndex = 1;
+            this.program_title.Text = "%title%";
+            // 
+            // open_file_button
+            // 
+            this.open_file_button.Location = new System.Drawing.Point(481, 95);
+            this.open_file_button.Name = "open_file_button";
+            this.open_file_button.Size = new System.Drawing.Size(121, 23);
+            this.open_file_button.TabIndex = 2;
+            this.open_file_button.Text = "Open file";
+            this.open_file_button.UseVisualStyleBackColor = true;
+            this.open_file_button.Click += new System.EventHandler(this.open_file_button_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.output_textbox);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(417, 194);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "LOGS";
+            // 
+            // output_textbox
+            // 
+            this.output_textbox.Location = new System.Drawing.Point(7, 20);
+            this.output_textbox.Multiline = true;
+            this.output_textbox.Name = "output_textbox";
+            this.output_textbox.ReadOnly = true;
+            this.output_textbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.output_textbox.Size = new System.Drawing.Size(404, 168);
+            this.output_textbox.TabIndex = 0;
+            // 
+            // description_label
+            // 
+            this.description_label.AutoSize = true;
+            this.description_label.Location = new System.Drawing.Point(20, 40);
+            this.description_label.Name = "description_label";
+            this.description_label.Size = new System.Drawing.Size(74, 13);
+            this.description_label.TabIndex = 4;
+            this.description_label.Text = "%description%";
+            // 
+            // copy_output_button
+            // 
+            this.copy_output_button.Enabled = false;
+            this.copy_output_button.Location = new System.Drawing.Point(481, 124);
+            this.copy_output_button.Name = "copy_output_button";
+            this.copy_output_button.Size = new System.Drawing.Size(121, 23);
+            this.copy_output_button.TabIndex = 5;
+            this.copy_output_button.Text = "Copy generated code";
+            this.copy_output_button.UseVisualStyleBackColor = true;
+            this.copy_output_button.Click += new System.EventHandler(this.copy_output_button_Click);
+            // 
+            // log_geneated_text_button
+            // 
+            this.log_geneated_text_button.AutoSize = true;
+            this.log_geneated_text_button.Location = new System.Drawing.Point(481, 153);
+            this.log_geneated_text_button.Name = "log_geneated_text_button";
+            this.log_geneated_text_button.Size = new System.Drawing.Size(141, 30);
+            this.log_geneated_text_button.TabIndex = 6;
+            this.log_geneated_text_button.Text = "Show generated code\r\n(degrades performance.)";
+            this.log_geneated_text_button.UseVisualStyleBackColor = true;
+            this.log_geneated_text_button.CheckedChanged += new System.EventHandler(this.log_geneated_text_button_CheckedChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.code_tab_page);
+            this.tabControl1.Location = new System.Drawing.Point(16, 67);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(443, 236);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(435, 210);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Logs";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // code_tab_page
+            // 
+            this.code_tab_page.Controls.Add(this.groupBox2);
+            this.code_tab_page.Location = new System.Drawing.Point(4, 22);
+            this.code_tab_page.Name = "code_tab_page";
+            this.code_tab_page.Padding = new System.Windows.Forms.Padding(3);
+            this.code_tab_page.Size = new System.Drawing.Size(435, 210);
+            this.code_tab_page.TabIndex = 1;
+            this.code_tab_page.Text = "Code";
+            this.code_tab_page.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.output_code_textbox);
+            this.groupBox2.Location = new System.Drawing.Point(6, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(417, 194);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Code";
+            // 
+            // output_code_textbox
+            // 
+            this.output_code_textbox.Enabled = false;
+            this.output_code_textbox.Location = new System.Drawing.Point(7, 20);
+            this.output_code_textbox.Multiline = true;
+            this.output_code_textbox.Name = "output_code_textbox";
+            this.output_code_textbox.ReadOnly = true;
+            this.output_code_textbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.output_code_textbox.Size = new System.Drawing.Size(404, 168);
+            this.output_code_textbox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.ForeColor = System.Drawing.Color.SlateBlue;
+            this.label1.Location = new System.Drawing.Point(588, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "by Seve267";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 315);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.log_geneated_text_button);
+            this.Controls.Add(this.copy_output_button);
+            this.Controls.Add(this.description_label);
+            this.Controls.Add(this.open_file_button);
+            this.Controls.Add(this.program_title);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "%title%";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.code_tab_page.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label program_title;
+        private System.Windows.Forms.Button open_file_button;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox output_textbox;
+        private System.Windows.Forms.Label description_label;
+        private System.Windows.Forms.Button copy_output_button;
+        private System.Windows.Forms.CheckBox log_geneated_text_button;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage code_tab_page;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox output_code_textbox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SBFD2EDSG_GUI/MainWindow.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SBFD2EDSG_GUI
+{
+    public partial class MainWindow : Form
+    {
+        public static string pb_program_name = "SBFD2EDSG_GUI";
+        public string pb_filePath = String.Empty;
+        public string pb_fileName = String.Empty;
+        public static bool codeGenerated = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            program_title.Text = pb_program_name;
+            this.Text = pb_program_name;
+            output_textbox.Text += pb_program_name + " Initialized.\r\n";
+            description_label.Text = "Sanny Builder Tool: File Data To Embedded Code Script Generator (GUI)";
+            output_code_textbox.Text = "'Show generated code' option is disabled.\r\nAutomatically cleared this log when enabled.";
+        }
+
+        private void WRITE_LOG_TEXT_TO_OUTPUT(string input)
+        {
+            output_textbox.Text += input;
+            Console.Write(output_textbox.Text);
+        }
+
+        private void WRITELINE_LOG_TEXT_TO_OUTPUT(string input)
+        {
+            output_textbox.Text += input + "\r\n";
+            Console.WriteLine(output_textbox.Text);
+        }
+
+        private void WRITE_CODE_LOG_TEXT_TO_OUTPUT(string input)
+        {
+            output_code_textbox.Text += input;
+            Console.Write(output_code_textbox.Text);
+        }
+
+        private void WRITELINE_CODE_LOG_TEXT_TO_OUTPUT(string input)
+        {
+            output_code_textbox.Text += input + "\r\n";
+            Console.WriteLine(output_code_textbox.Text);
+        }
+
+        private void open_file_button_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pb_filePath = openFileDialog.FileName;
+                    pb_fileName = openFileDialog.SafeFileName;
+                    WRITELINE_LOG_TEXT_TO_OUTPUT("File opened: " + pb_filePath);
+
+                    //LoadingForm loadingForm = new LoadingForm();
+                    ///loadingForm.ShowDialog();
+
+                    if (!BEGIN_PROCESS(pb_filePath, pb_fileName))
+                    {
+                        WRITELINE_LOG_TEXT_TO_OUTPUT("Failed, Empty file.");
+                    }
+
+                    copy_output_button.Enabled = true;
+
+                    if (log_geneated_text_button.Checked == true)
+                    {
+                        WRITELINE_CODE_LOG_TEXT_TO_OUTPUT(output_buffer);
+                    }
+
+                    codeGenerated = true;
+                    //loadingForm.
+
+                }
+
+            }
+        }
+
+        public static string output_buffer;
+        public static int output_buffer_len;
+        private static int count; // max 15
+
+        public bool BEGIN_PROCESS(string filePath, string fileName)
+        {
+            string a_output_buffer = String.Empty;
+            a_output_buffer += ":" + fileName;
+            output_buffer = a_output_buffer.Replace('.', '_');
+            output_buffer = _NEXT_LINE(output_buffer);
+            output_buffer += "hex";
+            output_buffer = _NEXT_LINE(output_buffer);
+            string b_output_buffer = _READ_DATA_TO_STRING(filePath);
+
+            if (b_output_buffer == "")
+            {
+                return false;
+            }
+
+            output_buffer += b_output_buffer;
+            output_buffer = _NEXT_LINE(output_buffer);
+            output_buffer += "end";
+
+            
+            //WRITELINE_LOG_TEXT_TO_OUTPUT(output_buffer);
+            return true;
+        }
+
+        private string _READ_DATA_TO_STRING(string filePath)
+        {
+            string hexStringBuffer = String.Empty;
+
+            try
+            {
+                using (FileStream targetFile = new FileStream(filePath, FileMode.Open))
+                {
+                    #region ...Reading procedure code
+                    long fileLen = targetFile.Length;
+                    // File buffer
+                    byte[] buffer = new byte[fileLen];
+
+                    targetFile.Read(buffer, 0, (int)fileLen);
+
+                    
+
+                    for (int i = 0; i < fileLen; i++)
+                    {
+                        object a_buffer = buffer.GetValue(i);
+                        int number = Convert.ToInt32(a_buffer);
+                        string b_buffer;
+                        b_buffer = String.Format("{0:X}", number);
+
+                        if (count == 0)
+                        {
+                            hexStringBuffer += " ";
+                        }
+
+                        hexStringBuffer += b_buffer;
+                        hexStringBuffer += " ";
+
+                        if (count > 14)
+                        {
+                            hexStringBuffer += "\r\n";
+                            count = -1;
+                        }
+
+                        count += 1;
+                    }
+
+                    targetFile.Close();
+                    
+                    #endregion
+                }
+                
+            }
+            catch (Exception e)
+            {
+                WRITELINE_LOG_TEXT_TO_OUTPUT("Exception occured:");
+                WRITELINE_LOG_TEXT_TO_OUTPUT(e.Message);
+            }
+
+            return hexStringBuffer;
+        }
+
+        static string StringToHex(string input)
+        {
+            // Convert string to byte array
+            byte[] byteArray = Encoding.UTF8.GetBytes(input);
+
+            // Convert byte array to hexadecimal string
+            StringBuilder hexBuilder = new StringBuilder();
+            foreach (byte b in byteArray)
+            {
+                hexBuilder.Append(b.ToString("X2")); // X2 ensures two-digit representation
+            }
+
+            return hexBuilder.ToString();
+        }
+
+        private string _NEXT_LINE(string output_buffer)
+        {
+            output_buffer += "\r\n";
+            return output_buffer;
+        }
+
+        private void copy_output_button_Click(object sender, EventArgs e)
+        {
+            Clipboard.Clear();
+            Clipboard.SetText(output_buffer);
+        }
+
+        private void log_geneated_text_button_CheckedChanged(object sender, EventArgs e)
+        {
+            if (log_geneated_text_button.Checked == false)
+            {
+                output_code_textbox.Text = "'Show generated code' option is disabled.\r\nAutomatically cleared this log when enabled.";
+                output_code_textbox.Enabled = false;
+                return;
+            }
+
+            if (codeGenerated == true)
+            {
+                output_code_textbox.Text = String.Empty;
+                WRITE_CODE_LOG_TEXT_TO_OUTPUT(output_buffer);
+                output_code_textbox.Enabled = true;
+                return;
+            }
+
+            output_code_textbox.Text = String.Empty;
+            output_code_textbox.Enabled = true;
+        }
+    }
+}
